4e29645ff3d57c53953053051e966f950f92f804
Clayton <ccrenshaw2012@gmail.com>
2025-06-15 07:10:53 -0500

    htmx

--- app.py
+++ app.py
@@ -9,11 +9,14 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tributes.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+with app.app_context():
+    db.create_all()
+
 # Model
 class Tribute(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    date = db.Column(db.String(100), nullable=True)
+    date = db.Column(db.DateTime, nullable=True)
     message = db.Column(db.Text, nullable=False)
     author = db.Column(db.String(100), nullable=False)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
@@ -26,22 +29,24 @@ def index():
 def military_bio():
     return render_template('military_bio.html')
 
-# Routes
-@app.route('/tribute_wall', methods=['GET', 'POST'])
+@app.route('/tribute_wall')
 def tribute_wall():
-    if request.method == 'POST':
-        name = request.form['name']
-        date = datetime.now()
-        message = request.form['message']
-        author = request.form['author']
-        new_tribute = Tribute(name=name, date=date, message=message, author=author)
-        db.session.add(new_tribute)
-        db.session.commit()
-        return redirect(url_for('tribute_wall'))
-    
     tributes = Tribute.query.order_by(Tribute.timestamp.desc()).all()
     year = datetime.now().year
     return render_template('tribute_wall.html', tributes=tributes, year=year)
+
+@app.route('/add_tribute', methods=['POST'])
+def add_tribute():
+    name = request.form.get('name', 'Tribute')
+    message = request.form['message']
+    author = request.form['author']
+    date = datetime.now()
+
+    new_tribute = Tribute(name=name, message=message, author=author, date=date)
+    db.session.add(new_tribute)
+    db.session.commit()
+
+    return render_template('tribute_partial.html', tribute=new_tribute)
 
 if __name__ == '__main__':
     app.run(debug=True)
